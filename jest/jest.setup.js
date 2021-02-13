fd80fe3747979a0dadce0fef6c3c56130acc7334
untemps <6Mv2^2S6$2>
2021-02-13 18:06:13 +0100

    chore: Fix tests setup

---
+++ jest/jest.setup.js
@@ -0,0 +1,15 @@
+const { toBeInTheDocument, toHaveAttribute, toHaveStyle } = require('@testing-library/jest-dom/matchers')
+import '@testing-library/jest-dom/extend-expect'
+
+expect.extend({ toBeInTheDocument, toHaveAttribute, toHaveStyle })
+
+global.generateDOM = (text = '') => {
+	const containerElement = document.createElement('div')
+	const contentElement = document.createElement('span')
+	contentElement.setAttribute('id', 'foo')
+	containerElement.appendChild(contentElement)
+	const textNode = document.createTextNode(text)
+	contentElement.appendChild(textNode)
+	document.body.appendChild(containerElement)
+	return containerElement
+}
