a629a30465cf71edad8bc1febfcdd4aca5cb71b9
untemps <6Mv2^2S6$2>
2021-03-13 15:36:36 +0100

    feat: Add support for element modification (#9)

--- src/DOMObserver.js
+++ src/DOMObserver.js
@@ -6,11 +6,11 @@ class DOMObserver {
 		return new Promise((resolve, reject) => {
 			const el = document.querySelector(selector)
 
-			if (el) {
+			if (!!el) {
 				resolve(el)
 			}
 
-			const error = new Error(`Error: Element ${selector} cannot be found`)
+			const error = new Error(`Element ${selector} cannot be found`)
 			if (timeout > 0) {
 				this._timeout = setTimeout(() => {
 					this.unwait()
@@ -22,17 +22,57 @@ class DOMObserver {
 
 			this._observer = new MutationObserver((mutations) => {
 				mutations.forEach((mutation) => {
-					const nodes = [...Array.from(mutation.addedNodes), ...Array.from(mutation.removedNodes)]
-					for (let node of nodes) {
-						if (!!node.matches && node.matches(selector)) {
-							this.unwait()
-							resolve(node)
+					if (mutation.type === 'childList') {
+						const nodes = [...Array.from(mutation.addedNodes), ...Array.from(mutation.removedNodes)]
+						for (let node of nodes) {
+							if (!!node.matches && node.matches(selector)) {
+								this.unwait()
+								resolve(node)
+							}
 						}
 					}
 				})
 			})
 
-			this._observer.observe(document.documentElement, { childList: true, subtree: true })
+			this._observer.observe(document.documentElement, {
+				childList: true,
+				subtree: true,
+			})
+		})
+	}
+
+	async watch(selector, options = {}, timeout = 0) {
+		return new Promise((resolve, reject) => {
+			const el = document.querySelector(selector)
+
+			const error = new Error(`Element ${selector} has not been modified before timeout`)
+			if (timeout > 0) {
+				this._timeout = setTimeout(() => {
+					this.unwait()
+					reject(error)
+				}, timeout)
+			}
+
+			this._observer = new MutationObserver((mutations) => {
+				mutations.forEach((mutation) => {
+					if (
+						(mutation.type === 'attributes' && !options?.attributeName) ||
+						options?.attributeName === mutation.attributeName
+					) {
+						this.unwait()
+						resolve({
+							target: mutation.target,
+							attributeName: mutation.attributeName,
+							oldValue: mutation.oldValue,
+						})
+					}
+				})
+			})
+
+			this._observer.observe(el, {
+				attributes: true,
+				attributeOldValue: true,
+			})
 		})
 	}
 
