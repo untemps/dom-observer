923a37a7399430b445e73ef343ede846a18b675e
untemps <v.lebadezet@untemps.net>
2022-01-07 14:24:57 +0100

    chore: Refactor dev instance

---
+++ dev/src/elements.js
@@ -0,0 +1,89 @@
+import { addElement, createElement, modifyElement, destroyElement } from './utils'
+
+export const createTooltip = () => {
+	return createElement({
+		id: 'tooltip',
+		class: 'tooltip',
+		content: 'This tooltip is tracked continuously',
+	})
+}
+
+export const createCard = () => {
+	const card = createElement({
+		id: 'card',
+		class: 'card',
+		'aria-label': 'Dynamic card',
+	})
+
+	const closeButtonCell = createElement({
+		class: 'card_cell_close-button',
+	})
+	const closeButton = createElement({
+		tagName: 'button',
+		class: 'button card_close-button',
+	})
+	closeButton.onclick = () => destroyElement(card)
+	const closeButtonLabel = createElement({
+		content: 'Remove this element',
+	})
+	addElement(closeButtonLabel, closeButton)
+	addElement(closeButton, closeButtonCell)
+	addElement(closeButtonCell, card)
+
+	const imageCell = createElement({
+		class: 'card_cell_image',
+	})
+	const image = createElement({
+		tagName: 'img',
+		class: 'card_image',
+		alt: 'Card image',
+		src: 'https://picsum.photos/300',
+	})
+	addElement(image, imageCell)
+	addElement(imageCell, card)
+
+	const descriptionCell = createElement({
+		class: 'card_cell_description',
+	})
+	const description = createElement({
+		class: 'card_description',
+	})
+	const descriptionText1 = createElement({
+		tagName: 'p',
+		content: 'The addition of this card is tracked one-shot then rerun continuously to track changes and deletion',
+	})
+	const descriptionText2 = createElement({
+		tagName: 'p',
+		content: 'Click the bottom button to log attribute modifications',
+	})
+	addElement(descriptionText1, description)
+	addElement(descriptionText2, description)
+	addElement(description, descriptionCell)
+	addElement(descriptionCell, card)
+
+	const leftButtonCell = createElement({
+		class: 'card_cell_left-button',
+	})
+	const leftButton = createElement({
+		tagName: 'button',
+		class: 'button',
+		content: 'Change class',
+	})
+	leftButton.onclick = (e) => modifyElement(card, 'class', 'card card--modified')
+	addElement(leftButton, leftButtonCell)
+	addElement(leftButtonCell, card)
+
+	const rightButtonCell = createElement({
+		class: 'card_cell_right-button',
+	})
+	const rightButton = createElement({
+		tagName: 'button',
+		class: 'button',
+		content: 'Change aria-label',
+	})
+	rightButton.onclick = (e) => modifyElement(card, 'aria-label', 'Modified card')
+	addElement(rightButton, rightButtonCell)
+	addElement(rightButtonCell, card)
+
+	return card
+}
--- dev/src/index.js
+++ dev/src/index.js
@@ -1,99 +1,53 @@
 import { DOMObserver } from '../../src'
+
+import { createCard, createTooltip } from './elements'
+import { log } from './log'
+import { addElement, destroyElement } from './utils'
+
 import './index.css'
 
-const observer = new DOMObserver()
+const tooltip = createTooltip()
 
-let count = 0
-const targetEl = document.getElementById('container')
-const outputEl = document.getElementById('output')
+const cardTarget = document.querySelector('#container')
 
-const getItemId = (index) => {
-	return `item-${index + 1}`
-}
-
-const createElement = (id) => {
-	const addedRect = document.createElement('div')
-	addedRect.setAttribute('id', id)
-	addedRect.setAttribute('class', `item`)
-	addedRect.setAttribute('aria-label', 'Dynamic item')
-	const addedLab = document.createTextNode(id)
-	addedRect.appendChild(addedLab)
-
-	const deleteBtn = document.createElement('button')
-	deleteBtn.setAttribute('class', 'item_delete-button')
-	deleteBtn.onclick = () => removeElement(addedRect)
-	const deleteLab = document.createTextNode('x')
-	deleteBtn.appendChild(deleteLab)
-	addedRect.appendChild(deleteBtn)
-
-	const modifyBtn = document.createElement('button')
-	modifyBtn.setAttribute('class', 'item_modify-button')
-	modifyBtn.onclick = (e) => modifyElement(addedRect, 'class', 'item--modified')
-	const modifyLab = document.createTextNode('Modify class')
-	modifyBtn.appendChild(modifyLab)
-	addedRect.appendChild(modifyBtn)
-
-	const modify2Btn = document.createElement('button')
-	modify2Btn.setAttribute('class', 'item_modify2-button')
-	modify2Btn.onclick = (e) => modifyElement(addedRect, 'aria-label', 'Modified item')
-	const modify2Lab = document.createTextNode('Modify aria-label')
-	modify2Btn.appendChild(modify2Lab)
-	addedRect.appendChild(modify2Btn)
-
-	return addedRect
-}
-
-const addElement = (el) => {
+const addButton = document.querySelector('#add-button')
+addButton.addEventListener('mouseenter', () => {
+	const tooltipContainer = document.querySelector('#add-button')
+	addElement(tooltip, tooltipContainer)
+})
+addButton.addEventListener('mouseleave', () => {
+	destroyElement(tooltip)
+})
+addButton.addEventListener('click', () => {
+	destroyElement(tooltip)
+	const el = document.querySelector('#card')
 	if (!el) {
-		el = createElement(getItemId(count++))
+		const card = createCard()
+		addElement(card, cardTarget)
 	}
-	targetEl.appendChild(el)
-}
-
-const removeElement = (el) => {
-	el.parentNode.removeChild(el)
-}
-
-const modifyElement = (el, attributeName, attributeNewValue) => {
-	el.setAttribute(attributeName, attributeNewValue)
-}
-
-const log = (message, values = null) => {
-	outputEl.value += `${message}`
-	if (!!values) {
-		outputEl.value += `\n\t{`
-		for (let z in values) {
-			outputEl.value += `\n\t\t${z}: '${values[z]}'`
-		}
-		outputEl.value += `\n\t}`
-	}
-	outputEl.value += `\n`
-}
-
-const btn = document.getElementById('add-button')
-btn.onclick = () => {
-	const el = document.querySelector('#new-element')
-	if (!!el) {
-		removeElement(el)
-	} else {
-		addElement(createElement('new-element'))
-	}
-}
+})
 
 const onEvent = (node, event, { attributeName } = {}) => {
 	switch (event) {
 		case DOMObserver.ADD: {
-			log(`Element ${node.id} added`)
+			log(`[ADD]\t\tElement id: ${node.id}`)
 			break
 		}
 		case DOMObserver.REMOVE: {
-			log(`Element ${node.id} removed`)
+			log(`[REMOVE]\tElement id: ${node.id}`)
 			break
 		}
 		default: {
-			log(`Attribute ${attributeName} for element ${node.id} changed`)
+			log(`[CHANGE]\tElement id: ${node.id} - Attribute: ${attributeName}`)
 		}
 	}
 }
 
-observer.wait(`#new-element`, onEvent)
+const tooltipObserver = new DOMObserver()
+tooltipObserver.wait(tooltip, onEvent, { events: [DOMObserver.ADD, DOMObserver.REMOVE] })
+
+const cardObserver = new DOMObserver()
+cardObserver.wait(`#card`).then(({ node }) => {
+	log(`[ADD]\t\tElement id: ${node.id}`)
+	new DOMObserver().wait(`#card`, onEvent, { events: [DOMObserver.REMOVE, DOMObserver.CHANGE] })
+})
